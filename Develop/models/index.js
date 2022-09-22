5bc61cc6827f60d1009ac4a7f4e785df106c370a
GranSpencer09 <Granspencer09@gmail.com>
2022-09-21 19:31:34 -0700

    set up database models and associations between them

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,28 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Painting.belongsTo(Category, {
+  foreignKey: "id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Category.belongToMany(Tag, {
+  through: { model: ProductTag },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: { model: ProductTag },
+});
 
 module.exports = {
   Product,
